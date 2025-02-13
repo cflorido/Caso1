29a2de8d90814c20cd9ce71464f871a7faacaab5
Mateo <m.parrao@uniandes.edu.co>
2025-02-13 18:36:22 -0500

    Revert

--- src/BuzonRevision.java
+++ src/BuzonRevision.java
@@ -13,27 +13,25 @@ class BuzonRevision {
     public synchronized void depositar(Producto producto) throws InterruptedException {
         System.out.println("se depositó en buzon de revision " + producto.getNombre());
         while (productos.size() >= capacidad) {
-            try {
-                wait(); // Espera pasiva si el buzón está lleno
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+            try{
+
+            
+                wait(); }
+                catch(InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
         }
         productos.add(producto);
-        notifyAll(); 
+        notifyAll(); // Notifica a los hilos en espera
     }
 
-    public Producto retirar() throws InterruptedException {
+    public synchronized Producto retirar() throws InterruptedException {
         while (productos.isEmpty()) {
-            Thread.yield(); // Espera semi-activa
-            Thread.sleep(100); 
+            wait(); // Espera pasiva si está vacío
         }
-
-        synchronized (this) {
-            Producto producto = productos.poll();
-            notifyAll(); // Notifica a los hilos en espera
-            return producto;
-        }
+        Producto producto = productos.poll();
+        notifyAll(); // Notifica a los hilos en espera
+        return producto;
     }
 
     public synchronized boolean estaVacio() {
